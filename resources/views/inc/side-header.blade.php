2491736c08cd4b2638d66ddcd23a411b6c1b0b8f
oleg <oleg24555535@gmail.com>
2021-04-22 15:40:50 +0300

    базовая верстка стартовой страницы

---
+++ resources/views/inc/side-header.blade.php
@@ -0,0 +1,40 @@
+<div class="col-md-4 col-lg-3 navbar-container fixed">
+<!-- Вертикальное меню -->
+<nav class="navbar navbar-expand-md ">
+    <div class="logo">
+        <a class="navbar-brand" href="#">
+        <img class="rounded img-fluid" src="\image\logo-removebg-preview (1).png"  alt="" >
+    </a>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbar">
+        <!-- Пункты вертикального меню -->
+        <ul class="navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link font-menu" href="#link-1">About us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-menu" href="#link-2">Новости</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-menu" href="#link-3">Вакансии</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-menu" href="#link-4">Курсы</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link font-menu" href="#link-5">Оставить резюме</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</div>
+
+
+
+
+
+
+
