9dca512167073442725de51109a724f7bce96bd2
oleg <oleg24555535@gmail.com>
2021-04-27 15:48:16 +0300

    изменена структура view. добавлен шаблон app и разбиение на секции. добавлена папка ru для мультиязычности

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,11 @@
+@extends('layouts.app')
 
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Найти работу - легко!</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@section('title')Найти работу - легко!@endsection
 
-        <link href="{{asset('\css\app.css')}}" rel="stylesheet" type="text/css">
-        <script src="/js/app.js" defer></script>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    </head>
-
-
-
-    <body>
-    <div class="container-fluid">
-    <div class="row">
-        @include('inc.side-header')
-
-        <div class="col-md-8 col-lg-9 content-container" >
-            @include ('inc.about-us')
-            @include('inc.news')
-            @include('inc.vacancies')
-            @include('inc.courses')
-            @include('inc.create-cv')
-        </div>
-
-
-
-
-    </div>
-    </div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
-</html>
+@section('content')
+    @include ('inc.about-us')
+    @include('inc.news')
+    @include('inc.vacancies')
+    @include('inc.courses')
+    @include('inc.create-cv')
+@endsection
